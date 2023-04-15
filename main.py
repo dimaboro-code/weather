e46e05cf895dc049772d098d4503373fc081b4eb
dimaboro <79355964+dimaboro-code@users.noreply.github.com>
2023-04-15 18:07:39 +0000

    Right form

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import requests
 
 
-cities = {
-  'London': '?MmnqT&lang=ru',
-  'Шереметьево': '?MmnqT&lang=ru', 
-  'Череповец': '?MmnqT&lang=ru'
+PAYLOAD = {
+  'M': '',
+  'm': '',
+  'n': '',
+  'q': '',
+  'T': '',
+  'lang': 'ru'
 }
-
-
-def get_weather(cities: dict):
-  url_form = 'https://wttr.in/{}{}'
   
-  for city in cities:
-    url = url_form.format(city, cities[city])
-    response = requests.get(url)
-    response.raise_for_status()
-    yield response.text
+CITIES = [
+  'London',
+  'Шереметьево',
+  'Череповец'
+]
 
+
+def get_weather(city):
+    url = 'https://wttr.in/' + city
+    response = requests.get(url, PAYLOAD)
+    response.raise_for_status()
+    return response.text
+
+
+def main():
+  for city in CITIES:
+    print(get_weather(city))
+  
 
 if __name__ == '__main__':
-  for weather in get_weather(cities):
-    print(weather)
+  main()
